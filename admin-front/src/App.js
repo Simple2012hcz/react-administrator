f13e3b6e628be7fd1c7900be50152f7effbb28bb
czhu2 <czhu2@iflytek.com>
2018-08-23 14:06:54 +0800

    左侧菜单底部可点击折叠

--- admin-front/src/App.js
+++ admin-front/src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CustomerRouter from "./router"
-import LeftBar from './share-components/leftBar/leftBar';
+import LeftBar from './share-components/leftBar-collapse-bottom/leftBar';
+
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
+
 
 class App extends Component {
+
   render() {
     return (
-     <div className="app-wrapper">
-        <LeftBar className="app-left-wrapper"/>
-        <CustomerRouter className="app-right-wrapper"/>
-     </div>
+      <Layout className="app-wrapper" >
+
+       <LeftBar />
+
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+          </Header>
+          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
     );
   }
 }
