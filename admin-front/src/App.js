3644d01088aeb72f4afaab940ed4f871fcba07e5
czhu2 <czhu2@iflytek.com>
2018-08-23 10:30:23 +0800

    add router

---
+++ admin-front/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CustomerRouter from "./router"
+
+class App extends Component {
+  render() {
+    return (
+      <CustomerRouter/>
+    );
+  }
+}
+
+
+export default App;
